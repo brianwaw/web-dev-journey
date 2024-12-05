097e95de6344cf04bb73de25f03c2b7163cbadea
brianwaw <bwaweru408@gmail.com>
2024-12-05 16:06:51 +0300

    polished asynchronous js

--- javascriptfiles/async.js
+++ javascriptfiles/async.js
@@ -1,5 +1,60 @@
-const url= 'https://jsonplaceholder.typicode.com/users';
+// function mockApiCall() {
+//     return new Promise((resolve) => {
+//         setTimeout(() => resolve('API Response'), 2000); // Simulates after 2 seconds
+//     });
+// }
 
-export function getData() {
-    fetch(url)
+// async function fetchData() {
+//     console.log('Fetching data...');
+//     const response = await mockApiCall();
+//     console.log(response); // Logs: 'API Response' after 2 seconds
+// }
+
+// fetchData()
+
+
+// excercise on fetching name
+
+// const fetchUsername = () => {
+//     return new Promise((resolve, reject) => {
+//         const success = true; // Simulate success or failure
+//         setTimeout(() => {
+//             if (success) {
+//                 resolve('Username found !!!')
+//             } else {
+//                 reject('Usename not found !!!')
+//             }
+//         }, 3000);
+//     });
+// };
+
+// fetchUsername()
+//     .then((message) => {
+//         console.log(message)
+//     })
+
+//     .catch((error) => {
+//         console.log(error)
+//     })
+
+
+// callback 
+
+function processOrder(orderId, callback) {
+    console.log(`Processing order ${orderId}`);
+
+    setTimeout(() => {
+        console.log(`Order ${orderId} Processed !`)
+
+        callback(orderId);
+    }, 3000);
 }
+
+function notifyUser(orderId) {
+    setTimeout(() => {
+        console.log(`notification sent to ${orderId}`)
+    }, 2000);
+}
+
+processOrder(100, notifyUser)
+notifyUser(200)
